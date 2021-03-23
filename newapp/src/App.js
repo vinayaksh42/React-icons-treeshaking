b7e62a3540b601b43a3581674429d6b1c29d3702
vinayaksh42 <vinayaksh42@gmail.com>
2021-03-24 00:10:30 +0530

    test app

---
+++ newapp/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import {Wifi ,Face } from 'react-eos-icons-vinayak-test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Wifi/>
+        <Face/>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
